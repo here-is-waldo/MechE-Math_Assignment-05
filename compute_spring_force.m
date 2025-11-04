98d5b1ac2fc7cab0fcbc4f8706050edc4ecc138b
lodio <lodio@olin.edu>
2025-11-04 16:00:01 -0500

    First file (unwritten function)

---
+++ compute_spring_force.m
@@ -0,0 +1,16 @@
+%computes the force exerted by the spring at one of its ends
+%INPUTS:
+%PA: the position of the first end of the spring
+%PB: the position of the second end of the spring
+%k: the stiffness of the spring
+%l0: the natural length of the spring
+%OUTPUTS:
+%F: the force exerted by the spring at end B
+function F = compute_spring_force(k,l0,PA,PB)
+%current length of the spring
+l = %your code here
+%unit vector pointing from PA to PB
+e_s = %your code here
+%Force exerted by spring at point B
+F = %your code here
+end
